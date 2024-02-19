91a864fd4a46844af311b91626f702c906cd74d9
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2024-02-19 22:48:47 +0530

    Update finance_tracker.go

--- finance_tracker.go
+++ finance_tracker.go
@@ -4,35 +4,71 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 // TODO: Define a struct for FinanceRecord with fields for amount, description, category, and type (income or expense)
 type FinanceRecord struct {
 	// Add fields here
+	amount      int
+	description string
+	category    string
+	rectype     string
 }
 
 // TODO: Define global slice to hold finance records
 var financeRecords []FinanceRecord
 
 // TODO: Implement function to add a finance record
-func addFinanceRecord(/* parameters */) {
+func addFinanceRecord(amount int, description string, category string, rectype string) {
 	// Implement function
+	var financeRecord = new(FinanceRecord)
+	financeRecord.amount = amount
+	financeRecord.description = description
+	financeRecord.category = category
+	financeRecord.rectype = rectype
+	financeRecords = append(financeRecords, *financeRecord)
 }
 
 // TODO: Implement function to print summary (total income, total expenses, net balance)
 func printSummary() {
 	// Implement function
+	var income int
+	var expenses int
+	for i := 0; i < len(financeRecords); i++ {
+		if financeRecords[i].rectype == "income" {
+			income += financeRecords[i].amount
+		} else {
+			expenses += financeRecords[i].amount
+		}
+	}
+	fmt.Println("income", income)
+	fmt.Println("expenses", expenses)
+	fmt.Println("net balance", income-expenses)
 }
 
 // TODO: Implement function to print records filtered by type (income/expense)
-func printRecordsByType(/* parameters */) {
+func printRecordsByType(rectype string) {
 	// Implement function
+	for i := 0; i < len(financeRecords); i++ {
+		if financeRecords[i].rectype == rectype {
+			fmt.Println(financeRecords[i].amount, financeRecords[i].description, financeRecords[i].category)
+		}
+	}
 }
 
 // TODO: Implement function for basic financial calculations (e.g., estimate next month savings)
-func calculateSavings(/* parameters */) {
+func calculateSavings( /* parameters */ ) {
 	// Implement function
+	var income int
+	var expenses int
+	for i := 0; i < len(financeRecords); i++ {
+		if financeRecords[i].rectype == "income" {
+			income += financeRecords[i].amount
+		} else {
+			expenses += financeRecords[i].amount
+		}
+	}
+	fmt.Println("Savings: ", income-expenses)
 }
 
 // TODO: Implement function to get user input and return as appropriate data type
@@ -57,11 +93,23 @@ func main() {
 		switch choice {
 		case "1":
 			// TODO: Implement adding a record
+			fmt.Println("enter details")
+			var amount int
+			var description string
+			var category string
+			var rectype string
+			fmt.Scanln(&amount, &description, &category, &rectype)
+			addFinanceRecord(amount, description, category, rectype)
 		case "2":
+			printSummary()
 			// TODO: Implement viewing summary
 		case "3":
+			var rectype string
+			fmt.Scanln(&rectype)
+			printRecordsByType(rectype)
 			// TODO: Implement viewing records by type
 		case "4":
+			calculateSavings()
 			// TODO: Implement calculating savings
 		case "5":
 			fmt.Println("Exiting...")
